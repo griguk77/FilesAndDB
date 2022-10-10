8df6b9c53114a1b73c75ef145f32c079b1daf694
griguk77 <griguk77@gmail.com>
2022-10-11 01:35:02 +0300

    Initial commit

---
+++ app/src/main/java/com/mycompany/myapplication6/MainActivity.java
@@ -0,0 +1,130 @@
+package com.mycompany.myapplication6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String FILE_NAME = "PR6";
+    private static final String FILE_EX_NAME = "PR6Ex";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    // сохранение файла
+    public void saveText(View view) {
+
+        FileOutputStream fos = null;
+        try {
+            EditText textBox = findViewById(R.id.editor);
+            String text = textBox.getText().toString();
+
+            fos = openFileOutput(FILE_NAME, MODE_PRIVATE);
+            fos.write(text.getBytes());
+            Toast.makeText(this, "Файл " + FILE_NAME + " сохранён", Toast.LENGTH_SHORT).show();
+        } catch (IOException ex) {
+
+            Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
+        } finally {
+            try {
+                if (fos != null)
+                    fos.close();
+            } catch (IOException ex) {
+
+                Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    // открытие файла
+    public void openText(View view) {
+
+        FileInputStream fin = null;
+        TextView textView = findViewById(R.id.text);
+        try {
+            fin = openFileInput(FILE_NAME);
+            byte[] bytes = new byte[fin.available()];
+            fin.read(bytes);
+            String text = new String(bytes);
+            textView.setText(text);
+        } catch (IOException ex) {
+
+            Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
+        } finally {
+
+            try {
+                if (fin != null)
+                    fin.close();
+            } catch (IOException ex) {
+
+                Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    public void deleteFile(View view) {
+        String filepath = "/data/data/com.mycompany.myapplication6/files/PR6";
+        File file = new File(filepath);
+        if (file.exists()) {
+            file.delete();
+            Toast.makeText(this, "Файл " + FILE_NAME + " удалён", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Файл " + FILE_NAME + " отсутствует", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private File getExternalPath() {
+        return new File(getExternalFilesDir(null), FILE_EX_NAME);
+    }
+
+    // сохранение файла
+    public void saveExText(View view) {
+
+        try (FileOutputStream fos = new FileOutputStream(getExternalPath())) {
+            EditText textBox = findViewById(R.id.editor);
+            String text = textBox.getText().toString();
+            fos.write(text.getBytes());
+            Toast.makeText(this, "Файл сохранен", Toast.LENGTH_SHORT).show();
+        } catch (IOException ex) {
+
+            Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    // открытие файла
+    public void openExText(View view) {
+
+        TextView textView = findViewById(R.id.text);
+        File file = getExternalPath();
+        // если файл не существует, выход из метода
+        if (!file.exists()) return;
+        try (FileInputStream fin = new FileInputStream(file)) {
+            byte[] bytes = new byte[fin.available()];
+            fin.read(bytes);
+            String text = new String(bytes);
+            textView.setText(text);
+        } catch (IOException ex) {
+
+            Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void db(View view) {
+        Intent intent = new Intent(this, DB.class);
+        startActivity(intent);
+    }
+}
